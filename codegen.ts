af27ae75ac92b07b183ab33e178b5dd3004ceb82
harumaxy <harumaxy@gmail.com>
2024-04-01 14:06:20 +0900

    fix graphql SDL path

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "./src/schema.graphql",
+  schema: "./src/typeDefs.graphql",
   generates: {
     "./src/resolvers-types.ts": {
       config: {
